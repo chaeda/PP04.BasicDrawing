5bc7163df7852abb8eefb471e3e5d6350d65ed67
손영진 <sonkim1001@naver.com>
2018-09-20 17:24:31 +0900

    0920 PP05

---
+++ PP05.AnimatingSprite/Game.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "SDL.h"
+using namespace std;
+
+
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int pos,
+		int width, int height, bool fullscreen);
+
+	void render();
+	void update() {};
+	void handleEvents();
+	void clean();
+
+	bool running()
+	{
+		return m_bRunning;
+	}
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	bool m_bRunning;
+};
+#pragma once
+#pragma once
