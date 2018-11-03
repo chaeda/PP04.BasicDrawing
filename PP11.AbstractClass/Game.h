a2b1650b20f7e224aa4257f173f00d35ed9f3c5f
수빈 문 <bluestonedaz@naver.com>
2018-11-03 15:20:19 +0900

    PP12.Vector2D 10/25

--- PP11.AbstractClass/Game.h
+++ PP11.AbstractClass/Game.h
@@ -1,34 +1,24 @@
 #pragma once
-#include "SDL.h"
-#include "GameObject.h"
-#include <vector>
-#include <iostream>
 #include "Player.h"
 #include "Enemy.h"
-#include "TextureManager.h"
+#include <vector>
+
 class Game
 {
-private:
-	Game() { }
-	static Game* s_pInstance;
-	SDL_Window* m_pWindow;
-	SDL_Renderer* m_pRenderer;
-	std::vector<GameObject*> m_gameObjects;
-	GameObject* m_go;
-	GameObject* m_player;
-	GameObject* m_enemy;
-	bool m_bRunning;
-	int m_currentFrame;
 public:
-	~Game() { }
-	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
 	void render();
 	void update();
 	void handleEvents();
 	void clean();
-	void quit();
 	bool running() { return m_bRunning; }
+
+	/*GameObject m_go;
+	Player m_player;*/
 	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+
 	static Game* Instance()
 	{
 		if (s_pInstance == 0)
@@ -38,5 +28,27 @@ public:
 		}
 		return s_pInstance;
 	}
+
+private:
+	Game();
+	static Game* s_pInstance;
+
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+	int m_currentFrame;
+
+	std::vector<GameObject*> m_gameObjects;
+
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
+
+	//TextureManager m_textureManager;
+	//SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	//SDL_Rect m_sourceRectangle; // 
+	//SDL_Rect m_destinationRectangle; //
 };
+
+Game* Game::s_pInstance = 0;
 typedef Game TheGame;
